e7117ccb3d63a62d41304cb041a45d993f254355
joce <jocelyn.Fournier@gmail.com>
2014-06-17 00:00:49 +0200

    add ioncube and handle versions

--- extensions/ioncube/extension.py
+++ extensions/ioncube/extension.py
@@ -12,28 +12,28 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Codizy Extension
+"""ioncube Extension
 
-Downloads, installs and configures the Codizy extension for PHP
+Downloads, installs and configures the ioncube extension for PHP
 """
 import os
 import os.path
 import logging
 
 
-_log = logging.getLogger('codizy')
+_log = logging.getLogger('ioncube')
 
 
 DEFAULTS = {
-    "CODIZY_VERSION": "1.3",
-    "CODIZY_PACKAGE": "codizy-{CODIZY_VERSION}.tar.gz",
-    "CODIZY_DOWNLOAD_URL": "https://www.codizy.com/download/module/{CODIZY_PACKAGE}",
-    "CODIZY_STRIP": False
+    "IONCUBE_VERSION": "4.6.1",
+    "IONCUBE_PACKAGE": "ioncube_loaders_lin_x86-64-{ioncube_VERSION}.tar.gz",
+    "IONCUBE_DOWNLOAD_URL": "https://www.ioncube.com/download/module/{ioncube_PACKAGE}",
+    "IONCUBE_STRIP": False
 }
 
 
 
-class CodizyInstaller(object):
+class IoncubeInstaller(object):
     def __init__(self, ctx):
         self._log = _log
         self._ctx = ctx
@@ -42,28 +42,31 @@ class CodizyInstaller(object):
             self._log.info("Initializing")
             self._merge_defaults()
             self._load_php_info()
-            self._load_codizy_info()
+            self._load_ioncube_info()
         except Exception:
-            self._log.exception("Error installing Codizy module! "
-                                "Codizy module will not be available.")
+            self._log.exception("Error installing Ioncube module! "
+                                "Ioncube module will not be available.")
 
     def _merge_defaults(self):
         for key, val in DEFAULTS.iteritems():
             if key not in self._ctx:
                 self._ctx[key] = val
 
-    def _load_codizy_info(self):
-        codizy_so_name = 'codizy-%s.so' % (self._php_api)
-        self.codizy_so = os.path.join('@{HOME}', 'codizy',
-                                        codizy_so_name)
-        self._log.info("PHP Extension [%s]", self.codizy_so)
+    def _load_ioncube_info(self):
+        if (self._php_api >= 20121212):
+            ioncube_so_name = 'ioncube/ioncube_loader_lin_5.5.so'
+        else:
+            ioncube_so_name = 'ioncube/ioncube_loader_lin_5.4.so'
+        self.ioncube_so = os.path.join('@{HOME}', 'ioncube',
+                                        ioncube_so_name)
+        self._log.info("PHP Extension [%s]", self.ioncube_so)
 
     def _load_php_info(self):
         self.php_ini_path = os.path.join(self._ctx['BUILD_DIR'],
                                          'php', 'etc', 'php.ini')
         self._php_extn_dir = self._find_php_extn_dir()
         self._php_api, self._php_zts = self._parse_php_api()
-        self._php_arch = self._ctx.get('CODIZY_ARCH', 'x64')
+        self._php_arch = self._ctx.get('ioncube_ARCH', 'x64')
         self._log.info("PHP API [%s] Arch [%s]",
                         self._php_api, self._php_arch)
 
@@ -83,12 +86,12 @@ class CodizyInstaller(object):
     def modify_php_ini(self):
         with open(self.php_ini_path, 'rt') as php_ini:
             lines = php_ini.readlines()
-        extns = [line for line in lines if line.startswith('extension=')]
+        extns = [line for line in lines if line.startswith('zend_extension=')]
         if len(extns) > 0:
             pos = lines.index(extns[-1]) + 1
         else:
-            pos = lines.index('#{PHP_EXTENSIONS}\n') + 1
-        lines.insert(pos, 'extension=%s\n' % self.codizy_so)
+            pos = lines.index('#{ZEND_EXTENSIONS}\n') + 1
+        lines.insert(pos, 'zend_extension=%s\n' % self.ioncube_so)
         lines.append('\n')
         with open(self.php_ini_path, 'wt') as php_ini:
             for line in lines:
@@ -109,10 +112,10 @@ def service_environment(ctx):
 
 
 def compile(install):
-    codizy = CodizyInstaller(install.builder._ctx)
-    _log.info("Installing Codizy module")
-    install.package('CODIZY')
-    _log.info("Configuring Codizy module in php.ini")
-    codizy.modify_php_ini()
-    _log.info("Codizy module Installed.")
+    ioncube = ioncubeInstaller(install.builder._ctx)
+    _log.info("Installing ioncube module")
+    install.package('ioncube')
+    _log.info("Configuring ioncube module in php.ini")
+    ioncube.modify_php_ini()
+    _log.info("ioncube module Installed.")
     return 0
