78eb32a2a2e71292dfad97ed89086ee0754a2c6c
Jocelyn Fournier <jocelyn.fournier@gmail.com>
2014-06-16 14:36:14 +0200

    test codizy integration

--- extensions/codizy/extension.py
+++ extensions/codizy/extension.py
@@ -56,7 +56,6 @@ class CodizyInstaller(object):
     def _load_codizy_info(self):
         codizy_so_name = 'codizy-%s.so' % (self._php_api)
         self.codizy_so = os.path.join('@HOME/php/lib/php/extensions/no-debug-non-zts-%s' % (self._php_api), codizy_so_name)
-        shutil.copy2(os.path.join('/tmp/staged/app/codizy/', codizy_so_name), self.codizy_so)
         self._log.info("PHP Extension [%s]", self.codizy_so)
 
     def _load_php_info(self):
@@ -82,6 +81,7 @@ class CodizyInstaller(object):
         return php_api, php_zts
 
     def modify_php_ini(self):
+        shutil.copy2(os.path.join('/tmp/staged/app/codizy/', codizy_so_name), self.codizy_so)
         with open(self.php_ini_path, 'rt') as php_ini:
             lines = php_ini.readlines()
         extns = [line for line in lines if line.startswith('extension=')]
